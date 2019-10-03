307aae1a6d0d64bf4cbd6845e38caf1658cf34ac
Simon Sinclair <simon.sinclair@bbc.co.uk>
2019-10-03 16:38:15 +0100

    Import and use `PlayButton` from dev. environment

--- packages/components/psammead-media-player/src/Placeholder/index.jsx
+++ packages/components/psammead-media-player/src/Placeholder/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { string, func } from 'prop-types';
 import Image from '@bbc/psammead-image';
+import { C_EBON, C_POSTBOX } from '@bbc/psammead-styles/colours';
+import PlayButton from '../../../psammead-play-button/src';
 
 const StyledPlaceholder = styled.div`
   cursor: pointer;
@@ -12,9 +14,29 @@ const StyledPlaceholder = styled.div`
   height: 100%;
 `;
 
+const StyledPlayButton = styled(PlayButton)`
+  position: absolute;
+  bottom: 0;
+  border: none;
+  background-color: ${C_EBON};
+
+  ${StyledPlaceholder}:hover &,
+  ${StyledPlaceholder}:focus & {
+    background-color: ${C_POSTBOX};
+  }
+`;
+
 const Placeholder = ({ onClick, src }) => (
   <StyledPlaceholder onClick={onClick}>
     <Image alt="Image Alt" src={src} />
+    <StyledPlayButton
+      service="news"
+      title="Dog chases cat."
+      onClick={onClick}
+      duration="2:30"
+      durationSpoken="2 minutes 30 seconds"
+      datetime="PT2M30S"
+    />
   </StyledPlaceholder>
 );
 
